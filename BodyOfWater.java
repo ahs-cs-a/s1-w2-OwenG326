7cfb4e1d16e0e1146f51b0ee39f9d58b25a407bf
Owen <owengaetagordon@outlook.com>
2025-08-22 14:23:14 -0700

    Homeworkv1

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -13,4 +13,16 @@ public BodyOfWater(String n, int a){
     name = n;
     avgDepth = a;
 }
+public void setName(String n){
+    name = n;
+}
+public void setLargestDiameter(int l){
+    largestDiameter = l;
+}
+public void printState(){
+    System.out.println(name);
+    System.out.println(largestDiameter);
+    System.out.println(avgDepth);
+    System.out.println(isSaltWater);
+}
 }
--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,6 +1,11 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater(null, 0);
-        System.out.println("here");
+        BodyOfWater b = new BodyOfWater("Lake", 12);
+        b.printState();
+        BodyOfWater b1 = new BodyOfWater("Ocean", 10, 10, true);
+        b1.printState();
+        b.setName("Puddle");
+        b.printState();
+        b1.printState();
     }
 }
