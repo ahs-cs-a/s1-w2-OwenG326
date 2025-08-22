bff481361e8efc9217738e3765aebcda9b0a06f8
Owen <owengaetagordon@outlook.com>
2025-08-22 13:48:31 -0700

    Homework1

--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,6 +1,6 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater();
+        BodyOfWater b = new BodyOfWater(null, 0);
         System.out.println("here");
     }
 }
